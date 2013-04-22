8ad47c5f56c168acc2c27a9a23bf76e8142a8d29
fkelly <frank.kelly@nokia.com>
2013-04-22 15:34:56 -0400

    Helps to have all the files

--- places-api/src/main/java/com/nokia/scbe/hackathon/bestdayever/placesapi/App.java
+++ places-api/src/main/java/com/nokia/scbe/hackathon/bestdayever/placesapi/App.java
@@ -1,6 +1,10 @@
 package com.nokia.scbe.hackathon.bestdayever.placesapi;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,8 +17,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        
+    	Set<String> distinctCategories = new HashSet<String>();
         ApplicationContext context = new ClassPathXmlApplicationContext(
                 "classpath*:**/services.xml");
 
@@ -22,9 +25,29 @@ public class App
         PlacesAPIClient client = clientFactory.createPlacesAPIClient();
         
         PlacesAPIService service = new PlacesAPIService(client);
-        List<PlaceResultItem> list = service.getPlacesNearHere(42.3821,-71.0244, 10);
+        List<PlaceResultItem> list = service.getPlacesNearHere(42.3821,-71.0244, 100);
         
         System.out.println("Got "+list.size()+" results back");
+        int i=1;
+        for(PlaceResultItem p: list)
+        {
+        	
+        	System.out.println("Place "+i+" : "+p.getTitle() +" is of type "+ (p.getCategory() == null ? "NO CAT" : p.getCategory().getTitle()));
+        	i++;
+        	if(p.getCategory()!=null)
+        	{
+        		String categoryTitle = p.getCategory().getTitle();
+        		if(!distinctCategories.contains(categoryTitle))
+        		{
+        			distinctCategories.add(categoryTitle);
+        		}
+        	}
+        }
+        
+        System.out.println("\nDistinct Categories::");
+        for (String s : distinctCategories) {
+            System.out.println(s);
+        }
         
     }
 }
---
+++ places-api/src/main/java/com/nokia/scbe/hackathon/bestdayever/placesapi/ClientFactory.java
@@ -0,0 +1,6 @@
+package com.nokia.scbe.hackathon.bestdayever.placesapi;
+
+public interface ClientFactory {
+
+	  PlacesAPIClient createPlacesAPIClient();
+}
